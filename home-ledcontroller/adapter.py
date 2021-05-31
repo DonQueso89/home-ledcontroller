ffdc3045cee5ae6c24d4eb174044d76caf699849
DonQueso89 <kg.v.ekeren@gmail.com>
2021-05-31 10:32:29 +0200

    fix NeoPixel

--- home-ledcontroller/adapter.py
+++ home-ledcontroller/adapter.py
@@ -8,7 +8,7 @@ try:
     import neopixel
     import board
 
-    pixels = neopixel.Neopixel(getattr(board, settings.pixel_pin), 50, brightness=.5, auto_write=False, pixel_order=getattr(neopixel, settings.color_order))
+    pixels = neopixel.NeoPixel(getattr(board, settings.pixel_pin), 50, brightness=.5, auto_write=False, pixel_order=getattr(neopixel, settings.color_order))
 except (NotImplementedError, ModuleNotFoundError):
     logger.debug("No Neopixel devices found, mocking CircuitPython objects")
     from unittest import mock
